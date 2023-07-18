2e7210f0e2b4e68e0f3e5f156478d9b5f7c65d8b
Dastin Darmawan <saymedastin@gmail.com>
2023-07-18 10:39:01 +0700

    cube rotation

---
+++ src/js/scripts.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+// camera.position.z = 5;
+// camera.position.y = 2;
+camera.position.set(0, 1, 5);
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate() {
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+
