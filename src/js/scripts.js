7552cdb0a7b7cf1f48f3ff90d4813b555be837a5
Dastin Darmawan <saymedastin@gmail.com>
2023-07-18 13:58:50 +0700

    add mouse rotation and zoom using OrbitControls

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -15,21 +16,24 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+
 const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
 
 // camera.position.z = 5;
 // camera.position.y = 2;
 camera.position.set(0, 1, 5);
+orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
-function animate() {
-  box.rotation.x += 0.01;
-  box.rotation.y += 0.01;
+function animate(time) {
+  box.rotation.x = time / 500;
+  box.rotation.y = time / 500;
   renderer.render(scene, camera);
 }
 
